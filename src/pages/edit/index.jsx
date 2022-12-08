f5c4bc37f539e2179e4ec21e0527a0aa4661d476
lucassss-li <lucas8qwer@gmail.com>
2022-12-08 14:49:57 +0800

    pref:输入优化

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './index.scss'
 import Button from '@/components/button'
 
@@ -7,6 +7,7 @@ export default class Edit extends Component {
     id: 0,
     content: '',
   }
+  inputRef = createRef()
   handleChange = event => {
     this.setState({ content: event.target.value })
   }
@@ -15,6 +16,7 @@ export default class Edit extends Component {
     this.props.history.push('/')
   }
   componentDidMount() {
+    this.inputRef.current.focus()
     const id = +this.props.match.params.id
     const data = this.props.list.find(item => item.id === id)
     if (!id || !data) return
@@ -26,7 +28,13 @@ export default class Edit extends Component {
   render() {
     return (
       <div className='editor'>
-        <input className='input' type='text' value={this.state.content} onChange={this.handleChange} />
+        <input
+          className='input'
+          type='text'
+          ref={this.inputRef}
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
         <Button onClick={this.handleSubmit}>ok</Button>
       </div>
     )
